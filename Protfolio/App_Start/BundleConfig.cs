490474072219578f3061221e687b64a8bb2867f9
BH <BH@DESKTOP-UQTM97C>
2020-02-26 09:19:58 +0600

    First Commit

--- Protfolio/App_Start/BundleConfig.cs
+++ Protfolio/App_Start/BundleConfig.cs
@@ -8,23 +8,30 @@ namespace Protfolio
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.min.css",
+                "~/Content/ionicons.min.css",
+                "~/Content/owl.carousel.min.css",
+                "~/Content/magnific-popup.css",
+                "~/Content/hover.min.css",
+                "~/Content/css/style.css",
+                "~/Content/css/responsive.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                "~/Scripts/jquery-3.3.1.js",
+                "~/Scripts/jquery-migrate.min.js",
+                "~/Scripts/bootstrap.bundle.min.js",
+                "~/Scripts/typed.js",
+                "~/Scripts/owl.carousel.min.js",
+                "~/Scripts/magnific-popup.min.js",
+                "~/Scripts/isotope.pkgd.min.js",
+                "~/Scripts/contactform.js",
+                "~/Scripts/main.js"));
+
+
         }
     }
 }
